136582131a2b068e2a0897a2e5874121e65e14c2
Hieu200405 <dohoanghieu7537@gmail.com>
2024-05-28 21:22:28 +0700

    Add files via upload

---
+++ Assets.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeMonkey {
+    public class Assets : MonoBehaviour {
+        private static Assets _i; 
+        public static Assets i {
+            get { 
+                if (_i == null) _i = (Instantiate(Resources.Load("CodeMonkeyAssets")) as GameObject).GetComponent<Assets>(); 
+                return _i; 
+            }
+        }
+        public Sprite s_White;
+
+    }
+
+}
