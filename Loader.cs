7f282c53ebcf5e726a8919199837c13cec17391d
Hieu200405 <dohoanghieu7537@gmail.com>
2024-05-28 21:30:19 +0700

    Update Loader.cs
    Them chu thich

--- Loader.cs
+++ Loader.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//Tai cac cua so cua game
 public static class Loader 
 {
    public enum Scene
