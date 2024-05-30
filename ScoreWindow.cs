d28ce41831cc2705040cda5d5316221567a7f49d
Hieu200405 <dohoanghieu7537@gmail.com>
2024-05-30 09:32:39 +0700

    Update ScoreWindow.cs

--- ScoreWindow.cs
+++ ScoreWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class ScoreWindow : MonoBehaviour
+//Thiet lap cua so tinh diem
 {
     private Text scoreText;
     private void Awake()
